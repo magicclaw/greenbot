ff724f0efc2f41b3d4a2ef47e44c92590e881b9e
Bob Scott <rscott@itriagehealth.com>
2016-09-26 20:31:48 -0600

    Fix bug with motor reversal mode

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const driveModule = require('./lib/drive')
 let drive = null
 
 board.on("ready", function () {
-  drive = driveModule(new five.Motor(['b5','b4','b3']), new five.Motor(['a5','a4','a3']), -1)
+  drive = driveModule(new five.Motor(['b5','b4','b3']), new five.Motor(['a5','a4','a3']), true)
 
   this.repl.inject({
     drive: drive
   })
 
-  drive.moveForward()
+  drive.moveForward(0.25)
   board.wait(1000, function () {
-    drive.moveBackward()
+    drive.moveBackward(0.25)
     board.wait(1000, function () {
       drive.stop()
     })
