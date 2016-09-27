c13cad7459a220eed12d21f589c7baf417aada90
Bob Scott <rscott@itriagehealth.com>
2016-09-26 20:05:09 -0600

    Refactoring

--- index.js
+++ index.js
@@ -5,79 +5,21 @@ const Tessel = require('tessel-io')
 const board = new five.Board({
   io: new Tessel()
 })
-const motors = [
-  {
-    motor: null,
-    velocity: 0
-  },
-  {
-    motor: null,
-    velocity: 0
-  }
-]
-const MAX_SPEED = 255
+const driveModule = require('./lib/drive')
+let drive = null
 
-board.on("ready", () => {
-  // MOTOR 0 == LEFT
+board.on("ready", function () {
+  drive = driveModule(new five.Motor(['b5','b4','b3']), new five.Motor(['a5','a4','a3']), -1)
 
-  motors[0].motor = new five.Motor(['a5','a4','a3'])
-  motors[1].motor = new five.Motor(['b5','b4','b3'])
+  this.repl.inject({
+    drive: drive
+  })
 
-  spinLeft(1)
-  board.wait(5000, () => {
-    spinRight(1)
-    board.wait(5000, () => {
-      vroom(0.75, 1)
-      board.wait(5000, () => {
-        vroom(-1, -0.75)
-        board.wait(5000, () => {
-          allStop()
-        })
-      })
+  drive.moveForward()
+  board.wait(1000, function () {
+    drive.moveBackward()
+    board.wait(1000, function () {
+      drive.stop()
     })
   })
 })
-
-function allStop () {
-  vroom(0, 0)
-}
-
-function spinLeft (speed) {
-  vroom(speed, -speed)
-}
-
-function spinRight (speed) {
-  vroom(-speed, speed)
-}
-
-function vroom (m1Velocity, m2Velocity) {
-  goMotor(0, m1Velocity)
-  goMotor(1, m2Velocity)
-}
-
-function goMotor (motorIndex, velocity) {
-  velocity = -velocity
-  const mData = motors[motorIndex]
-  velocity = Math.round(velocity * MAX_SPEED)
-  if (velocity === 0) {
-    mData.motor.stop()
-    mData.velocity = 0
-    return
-  }
-  if (velocity > 0) {
-    if (mData.velocity < 0) {
-      goMotor(motorIndex, 0)
-    }
-    mData.motor.forward(velocity)
-    mData.velocity = velocity
-    return
-  }
-  if (velocity < 0) {
-    if (mData.velocity > 0) {
-      goMotor(motorIndex, 0)
-    }
-    mData.motor.reverse(Math.abs(velocity))
-    mData.velocity = velocity
-    return
-  }
-}
