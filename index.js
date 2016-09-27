342bed8353a44724151c3e0d0bc4d639d0a5c890
Hpaulson <technicalhannah@gmail.com>
2016-09-27 13:12:43 -0600

    push

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ const board = new five.Board({
 const driveModule = require('./lib/drive')
 let drive = null
 
-board.on("ready", function () {
-  drive = driveModule(new five.Motor(['b5','b4','b3']), new five.Motor(['a5','a4','a3']), true)
+board.on('ready', function () {
+  drive = driveModule(new five.Motor(['b5', 'b4', 'b3']), new five.Motor(['a5', 'a4', 'a3']), true)
+  var photo1 = new five.Light('b1')
+  var photo2 = new five.Light('b2')
 
   this.repl.inject({
     drive: drive
@@ -23,6 +25,30 @@ board.on("ready", function () {
     })
   })
 
-  var photo = new five.Light('b2')
-  photo.on('change', () => console.log(photo.level))
+  // photo1.on('change', () => console.log('photo1: ', photo1.level))
+  // photo2.on('change', () => console.log('photo2: ', photo2.level))
+
+  // let proximity = new five.Proximity({
+  //   controller: 'HCSR04',
+  //   pin: 'a2'
+  // })
+  // proximity.on('data', function() {
+  //   console.log('Proximity: ')
+  //   console.log('  cm  : ', this.cm)
+  //   console.log('  in  : ', this.in)
+  //   console.log('-----------------')
+  // })
+  // proximity.on('change', function() {
+  //   console.log('The obstruction has moved.')
+  // })
+
+  console.log('ready')
+  var proximity = new five.Proximity({
+    controller: 'HCSR04',
+    pin: 'a2'
+  })
+  proximity.on("data", function () {
+    console.log("inches: ", this.inches)
+    console.log("cm: ", this.cm)
+  })
 })
